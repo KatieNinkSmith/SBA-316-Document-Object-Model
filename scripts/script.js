56b40c17d1e25d791e8b21adb1a2883e2aae697f
Katie Nink Smith <katiej.smith@outlook.com>
2024-11-10 08:59:21 -0700

    some big changes with layot and keeping it to one page

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,31 @@
 console.log("my script is linked!");
 
 const form = document.querySelector("form");
-console.log(form); // getting the whole form idk why i havent used it
-form.style.width = "50%";
+console.log(form); // getting the whole form to set a width
+form.style.width = "50%"; // setting width of the form within main
+const list = document.querySelector(".list");
+console.log(list);
+list.style.width = "50%";
 
 const formDiv = document.getElementById("contact");
 console.log(formDiv); // getting the form to push new elements into
 const email = document.getElementById("userEmail");
-console.log(email);
+console.log(email); // getting the email to pull email.value for storage
 const message = document.getElementById("userMessage");
-console.log(message);
+console.log(message); // getting the message to pull message.value for storage
 
 const emergency = document.querySelector(".emergency");
-console.log(emergency); //
+console.log(emergency); // emergency input so add event to the click
 
 emergency.addEventListener("submit", function (evt) {
+  // event toggles emergency button to red and does a pop up alert to confirm if your message is an emergency
   emergency.classList.toggle(
     "red"
     // alert(
     //   "If you would like to Submit this as an emergency it will be moved to the top of the que"
     // )
   );
+  //event is also supposed to save input data to push into a list
   // return {
   //   Name: userName.input,
   //   Phone: phone.input,
@@ -42,38 +47,46 @@ formDiv.prepend(phone); // push it into the form
 const label = document.createElement("label"); // create a <label> element.
 label.setAttribute("for", "phoneNumber"); // set the type
 // label.textContent = ["Please enter your phone number."];
-formDiv.prepend(label);
+formDiv.prepend(label); // push it into the form
 
-const userName = document.createElement("input");
-userName.setAttribute("type", "text");
-userName.setAttribute("required", "true0");
-userName.setAttribute("placeHolder", "Enter your name");
-formDiv.prepend(userName);
-// const userphone = phone();
-// console.log(userphone);
+const userName = document.createElement("input"); // create an <input> for a name
+userName.setAttribute("type", "text"); // set the type
+userName.setAttribute("required", "true"); // set to required
+userName.setAttribute("placeHolder", "Enter your name"); // set the place holder
+formDiv.prepend(userName); // push into the form
 
 phone.addEventListener("input", function (evt) {
+  // add event listener to the input that validates the phone number entered
   if (!/^\d+$/.test(phone.value)) {
     this.setCustomValidity("Enter only numbers for your phone number");
     return false;
-  }
+  } // must be numbers
   if (this.value.length < this.minLength) {
     this.setCustomValidity("Enter the correct length phone number");
     return false;
-  }
+  } // makes sure it is the length of a phone number
   if (this.value.length > this.maxLength) {
-    this.value = this.value.slice(0, this.maxLength); // does not allow phone if longer than max
+    this.value = this.value.slice(0, this.maxLength);
     return false;
-  }
+  } // does not allow input to be longer than max
   this.setCustomValidity(" ");
-  return true;
+  return true; // removes message
 });
+send.addEventListener("submit", function (evt) {
+  // add event to the send input
+  // return {
+  //   Name: userName.input,
+  //   Phone: phone.input,
+  //   Email: email.input,
+  //   Message: message.input,
+  //   // confirm("We will get back to you within 3 business days")
+  // };
+}); // supposed to return the input for storage
 
-// btn.addEventListener("click", function () {
-//   btn.forEach(() => {
+// btn.addEventListener("click", function () { // supposed to effect both inputs
+//   btn.forEach(() => { // create an li element for the input to be saved into
 //     const li = document.createElement("li");
-//     const input = document.querySelector("input");
-//     console.log(input.value);
+//     const storageInfo =
 //     if (input.value.trim() == "") return;
 //     li.textContent = input.value;
 //     document.querySelector("ul").appendChild(li);
@@ -81,24 +94,3 @@ phone.addEventListener("input", function (evt) {
 //   });
 // });
 // btn.addEventListener("click", handleBtnClick);
-
-// function handleBtnClick(evt) {
-//   const li = document.createElement("li");
-//   const input = document.querySelector("input");
-//   console.log(input.value);
-//   if (input.value.trim() == "") return;
-//   li.textContent = input.value;
-//   document.querySelector("ul").appendChild(li);
-//   input.value = "";
-// }
-// function textAreaCharacterCount(evt){
-//   evt.preventDefault();
-//   if (){
-
-//   }
-// }
-
-// const inputName = document.createElement("input"); // create an <input> element.
-// inputName.setAttribute("type", "text");// set the type
-// inputName.setAttribute("placeHolder", "Enter name"); // add additional attributes
-// formDiv.prepend(inputName); // push it into the form
