fb01611f92516f8f2304d36ea7cdb87c5f185c0d
Katie Nink Smith <katiej.smith@outlook.com>
2024-11-09 20:48:27 -0700

    lots of functionality changes its almost displaying messages correctly, i need to work on accepting inputbased on button clicked

--- README.md
+++ README.md
@@ -9,19 +9,21 @@ Since topic and content are secondary to functionality for this assessment, we h
 
 PLANNING
 Just one page
-toggle light and dark mode is a simple option for an event listener and event handler functions with user interaction changing the style i think ill use toggle to change color of the form background on the emergency form and ask are you sure you want tosubmit an emergency ticket.
+toggle light and dark mode is a simple option for an event listener and event handler functions with user interaction changing the style. I think ill use toggle to change color of the form background on the emergency form and ask are you sure you want to submit an emergency ticket.
 
 Requirements
-Cache at least one element using selectElementById.
+x--Cache at least one element using selectElementById.
 x--Cache at least one element using querySelector or querySelectorAll.
-Iterate over a collection of elements to accomplish some task.
-Create at least one element using createElement.
+**loop and add nav bar** Iterate over a collection of elements to accomplish some task.
+x--Create at least one element using createElement.
 x--Use appendChild and/or prepend to add new elements to the DOM.
-Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
-Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
-Modify at least one attribute of an element in response to user interaction.
-Register at least two different event listeners and create the associated event handler functions.
+x--Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
+x--Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
+x--Modify at least one attribute of an element in response to user interaction.
+x--Register at least two different event listeners and create the associated event handler functions.
 Use at least two Browser Object Model (BOM) properties or methods.
-Include at least one form and/or input with HTML attribute validation.
-Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
+x--Include at least one form and/or input with HTML attribute validation.
+x--Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
 Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit).
+
+make another page with a list that stores the name, phone, email and message. if they click send message it is added to the end of the list. if they click emergency message it is added to the top of the list highlighted yellow/red.
--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,80 @@
 console.log("my script is linked!");
 
 const form = document.querySelector("form");
-console.log(form);
-
-const formDiv = document.getElementById("contact");
-console.log(formDiv);
-
+console.log(form); // getting the whole form idk why i havent used it
 form.style.width = "50%";
 
-const input = document.createElement("input"); // create an <input> element
-input.addEventListener("input", validateInput);
+const formDiv = document.getElementById("contact");
+console.log(formDiv); // getting the next step in of my form also idk why i havent used it
 
-function validateInput(evt) {
-  evt.preventDefault();
-  if (input.value.length <= 0) {
-    this.setCustomValidity("Please enter phone number");
-    input.focus();
-    return false;
-  }
-  if (input.value !== "/^d+$/") {
+const emergency = document.querySelector(".white");
+console.log(emergency); //
+
+const input = document.createElement("input"); // create an <input> element
+input.setAttribute("type", "tel");
+input.setAttribute("placeHolder", "Enter you phone number");
+input.setAttribute("required", "true");
+input.setAttribute("minlength", "10");
+input.setAttribute("maxlength", "10");
+// input.setAttribute("pattern", "[^/^d+$/]");
+
+// input.setAttribute("requiered"); cant set attribute as required
+formDiv.prepend(input);
+
+input.addEventListener("input", function (e) {
+  if (!/^\d+$/.test(input.value)) {
     this.setCustomValidity("Enter only numbers for your phone number");
     return false;
   }
-  if (input.value.length > 10 || input.value.length < 10) {
-    this.setCustomValidity("Please enter a corret phone number length");
+  if (this.value.length < this.minLength) {
+    this.setCustomValidity("Enter the correct length phone number");
     return false;
   }
-  if (input.value.length === 10) {
-    this.setCustomValidity("");
+  if (this.value.length > this.maxLength) {
+    this.value = this.value.slice(0, this.maxLength); // Truncate if longer than max
+    return false;
+  }
+  if (this.value === 10) {
+    this.validity = valid;
+    console.log(this.validity);
     return true;
   }
-}
-input.setAttribute("type", "numbers");
-input.setAttribute("placeHolder", "Enter you phone number");
-// input.setAttribute("requiered"); cant set attribute as required
-formDiv.prepend(input);
+});
 
 const label = document.createElement("label"); // create a <label> element.
 label.setAttribute("for", "phoneNumber"); // set the type
 label.textContent = ["Please enter your phone number."];
 formDiv.prepend(label);
+
+// const userInput = input();
+// console.log(userInput);
+
+emergency.addEventListener("click", () => {
+  console.log(emergency.classList.toggle("white"));
+});
+
+input.addEventListener("onKeyDown", (evt) => {
+  evt.preventDefault();
+  if (input.value !== "/^d+$/") {
+    // evt.preventDefault();
+    this.setCustomValidity("Enter only numbers for your phone number");
+    return false;
+  } else if (this.value.length > this.maxLength) {
+    // evt.preventDefault();
+    this.value = this.value.slice(0, this.maxLength);
+    // this.value.setCustomValidity(" ");
+  }
+  return {
+    phone: "input.value",
+  };
+});
+
+// function textAreaCharacterCount(evt){
+//   evt.preventDefault();
+//   if (){
+
+//   }
+// }
 
 // const inputName = document.createElement("input"); // create an <input> element.
 // inputName.setAttribute("type", "text");// set the type
